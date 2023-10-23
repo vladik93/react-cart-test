eee69dd53b76478231f2646d8e4cd330b4f296be
Vladi <39379713+vladik93@users.noreply.github.com>
2023-10-24 00:38:02 +0300

    trying to fix cart array issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,54 @@
-import './App.css'
-import React, {useState, useEffect} from "react";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import products from "./products";
 
 export default function App() {
   const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
-    setCart(prevState => [...prevState, item]);
-  }
+    setCart((prevState) => [...prevState, item]);
+  };
 
   useEffect(() => {
     console.log(cart);
-  }, [cart])
+  }, [cart]);
 
-  const Product = ({id, title, cost, amountInStock}) => {
+  const Product = ({ id, title, cost, amountInStock }) => {
     const [quantity, setQuantity] = useState(0);
-  
+
     const onIncrement = () => {
-      setQuantity(prevState => {
-        if(prevState < amountInStock) {
+      setQuantity((prevState) => {
+        if (prevState < amountInStock) {
           return prevState + 1;
-        } else{
+        } else {
           return prevState;
         }
-      })
-    }
+      });
+    };
 
     const onDecrement = () => {
-      setQuantity(prevState => {
-        if(prevState > 0) {
+      setQuantity((prevState) => {
+        if (prevState > 0) {
           return prevState - 1;
-        } else{
+        } else {
           return prevState;
         }
-      })
-    }
+      });
+    };
 
     const handleAddToCart = () => {
-      if(quantity > 0) {
-        let item = {
-          id,
-          title,
-          cost, 
-          quantity
-        };
-
-        
-      } else {
-        return null;
-      }
-      
-    }
+      let item = {
+        id,
+        title,
+        cost,
+        quantity,
+      };
+    };
 
     useEffect(() => {
       handleAddToCart();
     }, [quantity]);
-    
+
     return (
       <div className="product">
         <h1>{title}</h1>
@@ -65,28 +59,59 @@ export default function App() {
           <button onClick={onIncrement}>+</button>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
-
-
-  
   return (
     <div className="app">
-    <div className="products">
-    <Product id={1} title="Product 1" cost={25} amountInStock={5} addToCart={addToCart}/>
-    <Product id={2} title="Product 2" cost={25} amountInStock={5} addToCart={addToCart}/>
-    <Product id={3} title="Product 3" cost={25} amountInStock={5} addToCart={addToCart}/>
-    <Product id={4} title="Product 4" cost={25} amountInStock={5} addToCart={addToCart}/>
-    </div>
-    <hr />
-  
-    <div>
-      <h1>My Cart</h1>
+      <div className="products">
+        <Product
+          key={1}
+          id={1}
+          title="Product 1"
+          cost={25}
+          amountInStock={5}
+          addToCart={addToCart}
+        />
+        <Product
+          key={2}
+          id={2}
+          title="Product 2"
+          cost={25}
+          amountInStock={5}
+          addToCart={addToCart}
+        />
+        <Product
+          key={3}
+          id={3}
+          title="Product 3"
+          cost={25}
+          amountInStock={5}
+          addToCart={addToCart}
+        />
+        <Product
+          key={4}
+          id={4}
+          title="Product 4"
+          cost={25}
+          amountInStock={5}
+          addToCart={addToCart}
+        />
+      </div>
+      <hr />
 
-
-      
+      <div>
+        <h1>My Cart</h1>
+        <ul>
+          {cart.map((cartItem) => {
+            return (
+              <li>
+                {cartItem.title} {cartItem.quantity}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
---
+++ src/products.js
@@ -0,0 +1,8 @@
+const products = [
+  { id: 1, title: "Product 1", cost: 25, amountInStock: 5 },
+  { id: 2, title: "Product 2", cost: 25, amountInStock: 5 },
+  { id: 3, title: "Product 3", cost: 25, amountInStock: 5 },
+  { id: 4, title: "Product 4", cost: 25, amountInStock: 5 },
+];
+
+export default products;
